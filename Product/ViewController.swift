2905fb187b2957d643b0f813e3e45d54b5c31a42
Navdeep Boparai <navdeeboparai@hotmail.com>
2019-05-31 21:10:16 -0400

    mac

--- Product/ViewController.swift
+++ Product/ViewController.swift
@@ -10,11 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstnumber: UITextField!
+    @IBOutlet weak var resultlbl: UILabel!
+    
+    @IBOutlet weak var secondnumber: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnClick(_ sender: Any) {
+        //print("Hello world")
+        let first = Double(firstnumber.text!)
+        let second = Double(secondnumber.text!)
+        
+        
+        let result = first!+second!
+        
+        resultlbl.text = String(result)
+        
+        
+    }
+    
+    
+    @IBAction func btnAdd(_ sender: Any) {
+        let first = Double(firstnumber.text!)
+        let second = Double(secondnumber.text!)
+        
+        let result = first! - second!
+        
+        resultlbl.text = String(result)
+        
+        
+        
+        
+    }
+    
+    @IBAction func btnDiv(_ sender: Any) {
+        
+        let first = Double(firstnumber.text!)
+        let second = Double(secondnumber.text!)
+        
+        let result = first! / second!
+        
+        resultlbl.text = String(result)
+        
+        
+        
+        
+    }
+    
+    
+    @IBAction func btnmul(_ sender: Any) {
+        
+        let first = Double(firstnumber.text!)
+        let second = Double(secondnumber.text!)
+        
+        let result = first! * second!
+        
+        resultlbl.text = String(result)
+        
+        
+        
+        
+    }
+    
 }
 
